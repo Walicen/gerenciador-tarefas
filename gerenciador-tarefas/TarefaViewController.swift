bc3ee6e2935410111fb35f27557beeb381062e3b
Walicen <walicen.r@gmail.com>
2019-11-10 23:22:34 -0300

    changing button save task

--- gerenciador-tarefas/TarefaViewController.swift
+++ gerenciador-tarefas/TarefaViewController.swift
@@ -11,39 +11,25 @@ import UIKit
 class TarefaViewController: UIViewController {
 
     @IBOutlet weak var taskDescription: UITextField!
+    
     let tarefaUserDefault = TarefaUserDefaults()
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem.init(barButtonSystemItem: .done, target: self, action: #selector(self.addTask))
+        
         print(tarefaUserDefault.listTasks())
         
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
  
-    @IBAction func addTask() {
-        
-        
-        if let tarefaTransient = taskDescription.text{
+    @objc func addTask() {
+        if let tarefaTransient = taskDescription.text {
             
             tarefaUserDefault.saveTask(tarefa: tarefaTransient)
-            taskDescription.text = ""
+            taskDescription.text?.removeAll()
         }
         navigationController?.popToRootViewController(animated: true)
     }
